db574907c7632ce65666f8abac6850c2cbf26ad0
AnubhavKumar1234 <kumaranubhav847@gmail.com>
2022-08-16 18:50:31 +0530

    Finally commited

---
+++ samplejenkins/src/test/java/sample1/JenkinTest.java
@@ -0,0 +1,29 @@
+package sample1;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JenkinTest 
+{
+	@Test
+	public void jenkinTest() throws Throwable
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		FileInputStream fis=new FileInputStream("./data.properties");
+		Properties p=new Properties();
+		p.load(fis);
+		String Url = p.getProperty("url");
+		driver.get(Url);
+		System.out.println(Url);
+		System.out.println("Campaign created");
+
+}
+
+}
